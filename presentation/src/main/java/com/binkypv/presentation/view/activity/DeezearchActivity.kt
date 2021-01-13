7f5e2c37ad06da4861d79c823a95d6dffaf159ff
binkypv <missingno770@gmail.com>
2021-01-13 16:43:22 +0100

    Basic project structure

---
+++ presentation/src/main/java/com/binkypv/presentation/view/activity/DeezearchActivity.kt
@@ -0,0 +1,13 @@
+package com.binkypv.presentation.view.activity
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.binkypv.presentation.R
+
+class DeezearchActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deezearch)
+    }
+}
